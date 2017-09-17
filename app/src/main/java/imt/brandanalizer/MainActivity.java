5474ebcb6facfa83387a2fbd255a5c71989a9c26
flotan11 <mardon.florian@gmail.com>
2017-09-17 23:01:29 +0200

    Camera gestion with an independant file system for our application, display of the picture just after snap it (bug on real device : the picture has not the good orientation and the button to retake a picture disappear (not happens on emulated devices)), and gestion of the rotation of screen (landscape and portrait).
    CodeName : Awaken Dragon !

--- app/src/main/java/imt/brandanalizer/MainActivity.java
+++ app/src/main/java/imt/brandanalizer/MainActivity.java
@@ -1,52 +1,100 @@
 package imt.brandanalizer;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
+import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import static android.content.Context.*;
+import static android.os.Environment.getExternalStoragePublicDirectory;
+import static android.support.v4.content.FileProvider.getUriForFile;
+import static java.io.File.createTempFile;
+
+@SuppressLint("SimpleDateFormat")
+public class MainActivity extends Activity {
+    Context context;
+    private static final int CAMERA_PIC_REQUEST = 001;
+    String mCurrentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        context = this.getApplicationContext();
+        if (savedInstanceState!=null && savedInstanceState.containsKey("picture")) {
+            mCurrentPhotoPath=savedInstanceState.getString("picture");
+            ImageView image = (ImageView) findViewById(R.id.imageView1);
+            image.setImageURI(Uri.parse(mCurrentPhotoPath));
+        }
+        Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
             }
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    private void dispatchTakePictureIntent() {
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                File photoFile = null;
+                try {
+                    photoFile = createImageFile();
+                } catch (IOException e) {
+                    Log.d("PERMISSION", e.getMessage());
+                }
+                if (photoFile != null) {
+                    Uri photoURI = getUriForFile(this,
+                            "com.brandanalizer.fileprovider",
+                            photoFile);
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                    startActivityForResult(takePictureIntent, CAMERA_PIC_REQUEST);
+
+    /*                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+                            Uri.fromFile(photoFile));
+                    startActivityForResult(takePictureIntent, CAMERA_PIC_REQUEST);*/
+                }
+            }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp+"_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);//getFilesDir();
+        File image = createTempFile(imageFileName,".jpg",storageDir);
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = "file:" +image.getAbsolutePath();
+        return image;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAMERA_PIC_REQUEST) {
+            ImageView image = (ImageView) findViewById(R.id.imageView1);
+            image.setImageURI(Uri.parse(mCurrentPhotoPath));
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    @Override
+    public void onSaveInstanceState(Bundle outState)
+    {
+        outState.putString("picture",mCurrentPhotoPath);
+        super.onSaveInstanceState(outState);
     }
 }
